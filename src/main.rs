afd871db1d581a144e1bc2be4176b00c00df111c
Cory Johnson <cory1993johnson@gmail.com>
2023-12-04 22:14:07 +0100

    first half of first day

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,22 @@
-// Day2 Problem:
-
-/*
---- Day 4: Scratchcards ---
-The gondola takes you up. Strangely, though, the ground doesn't seem to be coming with you; you're not climbing a mountain. As the circle of Snow Island recedes below you, an entire new landmass suddenly appears above you! The gondola carries you to the surface of the new island and lurches into the station.
-
-As you exit the gondola, the first thing you notice is that the air here is much warmer than it was on Snow Island. It's also quite humid. Is this where the water source is?
-
-The next thing you notice is an Elf sitting on the floor across the station in what seems to be a pile of colorful square cards.
-
-"Oh! Hello!" The Elf excitedly runs over to you. "How may I be of service?" You ask about water sources.
-
-"I'm not sure; I just operate the gondola lift. That does sound like something we'd have, though - this is Island Island, after all! I bet the gardener would know. He's on a different island, though - er, the small kind surrounded by water, not the floating kind. We really need to come up with a better naming scheme. Tell you what: if you can help me with something quick, I'll let you borrow my boat and you can go visit the gardener. I got all these scratchcards as a gift, but I can't figure out what I've won."
-
-The Elf leads you over to the pile of colorful cards. There, you discover dozens of scratchcards, all with their opaque covering already scratched off. Picking one up, it looks like each card has two lists of numbers separated by a vertical bar (|): a list of winning numbers and then a list of numbers you have. You organize the information into a table (your puzzle input).
-
-As far as the Elf has been able to figure out, you have to figure out which of the numbers you have appear in the list of winning numbers. The first match makes the card worth one point and each match after the first doubles the point value of that card.
-
-For example:
-
-Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have, four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8 points (1 for the first match, then doubled three times for each of the three matches after the first).
-
-Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
-Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
-Card 4 has one winning number (84), so it is worth 1 point.
-Card 5 has no winning numbers, so it is worth no points.
-Card 6 has no winning numbers, so it is worth no points.
-So, in this example, the Elf's pile of scratchcards is worth 13 points.
-
-Take a seat in the large pile of colorful cards. How many points are they worth in total?
-*/
-
 use std::env::current_dir;
+use std::fmt::format;
 use std::fs;
 use std::path::PathBuf;
 use std::str::FromStr;
 
 fn main() {
-    let mut all_games: Vec<Game> = vec![];
     let cwd = current_dir().unwrap();
-    let input_fp = PathBuf::from_str((cwd.to_str().unwrap().to_owned() + "\\src\\4\\cards").as_str());
+    let input_fp = PathBuf::from_str((cwd.to_str().unwrap().to_owned() + "\\src\\1\\input").as_str());
+    
     let raw_input = fs::read_to_string(input_fp.unwrap()).expect("File doesn't exist :'(");
 
-    // parse lines
-    for line in raw_input.lines() {
-        let space_idx = line.split(':').nth(0).unwrap().trim().find(' ').unwrap();
-        let id = &line.split(':').nth(0).unwrap()[space_idx..].trim().parse::<i32>().unwrap();
-        let mut winners: Vec<i32> = vec![];
-        let mut nums: Vec<i32> = vec![];
-        // println!("Game ID: {}",id);
-
-        //parse for winners
-        for num in line.split(':').nth(1).unwrap().split('|').nth(0).unwrap().trim().split(' ') {
-            if num.contains(char::is_numeric) {
-                winners.push(num.parse::<i32>().unwrap());
-            }
-        }
-
-        //parse for nums
-        for num in line.split(':').nth(1).unwrap().split('|').nth(1).unwrap().trim().split(' ') {
-            if num.contains(char::is_numeric) {
-                nums.push(num.trim().parse::<i32>().unwrap());
-            }
-        }
-        all_games.push(Game { id: *id, winners: winners, numbers: nums, score: None , multiplyer: None})
-    }
-
-    // calculate score
-    for game in all_games.iter_mut() {
-        game.calculate_score();
-    }
-    // println!("{:?}", all_games.clone());
-
-    // sum score
-    let mut sum = 0;
-    for game in all_games.iter_mut() {
-        sum += game.score.unwrap();
-    }
-    // println!("Sum is {}", sum);
-
-    // part 2 (card counting)
-    let mut card_counts: Vec<i32> = vec![1; 213];
-    let mut idx = 0;
-    // loop through cards and add to subsequent cards based on the current card count for this number
-    for card in all_games {
-        
-
-        idx +=1;
-    }
-
-}
-
-#[derive(Debug,Clone)]
-pub struct Game {
-    id: i32,
-    winners: Vec<i32>,
-    numbers: Vec<i32>,
-    score: Option<i32>,
-    multiplyer: Option<usize>,
-}
-
-impl Game {
-    pub fn calculate_score(&mut self) {
-        let mut score = 0;
-        let mut first_passed = false;
-
-        for num in self.numbers.clone() {
-            if self.winners.contains(&num) {
-                if !first_passed {
-                    score = 1;
-                    first_passed = true;
-                } else {
-                    score *= 2;
-                }
-            }
-        }
-        self.score = Some(score);
-    }
+    let sum = raw_input.lines().fold(0, |acc,l| {
+        let first_num_location = l.find(char::is_numeric).unwrap();
+        let last_num_location = l.rfind(char::is_numeric).unwrap();
+        let first_dig = &l.chars().nth(first_num_location).unwrap();
+        let second_dig = &l.chars().nth(last_num_location).unwrap();
+        let num = format!("{}{}",first_dig,second_dig).parse::<i32>().unwrap();
+        acc + num
+    });
+    println!("{}",sum);
 }
