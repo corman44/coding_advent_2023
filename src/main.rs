e2dd66d80c628ab2d47c0ef13f5efa411b55c716
Cory Johnson <cory1993johnson@gmail.com>
2023-12-08 13:36:05 +0100

    day 5: much improved with rayon but still not "fast"

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,156 @@
+use core::num;
+use std::env::current_dir;
+use std::ops::Range;
+use std::{fs, thread};
+use std::path::PathBuf;
+use std::str::FromStr;
+use rayon::prelude::*;
 
 fn main() {
-    println!("");
+    let cwd = current_dir().unwrap();
+    let input_fp = PathBuf::from_str((cwd.to_str().unwrap().to_owned() + "\\src\\5\\seeds").as_str());
+    
+    let raw_input = fs::read_to_string(input_fp.unwrap()).expect("File doesn't exist :'(");
+
+    let mut input_lines = raw_input.lines().into_iter();
+    let first_line = &input_lines.next().unwrap();
+    let seeds_str = first_line.split(':').nth(1).unwrap().trim().split(' ');
+    let seeds: Vec<i64> = seeds_str.into_iter()
+        .map(|x| x.trim().parse::<i64>().unwrap())
+        .collect();
+
+    let _= input_lines.next(); //skip next empty line
+    let mut all_maps: Vec<Mapping> = vec![];
+
+    // parse input into maps
+    for current_line in input_lines.clone() {
+        if current_line.contains("map") {
+            // parse source & dest names
+            let temp_source_name = current_line.split('-').nth(0).unwrap();
+            let temp_dest_name = current_line.split('-').nth(2).unwrap().trim().split(' ').nth(0).unwrap();
+            
+            // parse each source and dest mapping
+            let mut temp_mapping: Mapping = Mapping { source_name: temp_source_name.to_string(), dest_name: temp_dest_name.to_string(), maps: vec![]};
+
+            input_lines.next();
+            for each in input_lines.by_ref() {
+                if each.is_empty() {
+                    break;
+                }
+                if each.contains(char::is_numeric) {
+                    let parsed_nums: Vec<i64> = each
+                        .trim()
+                        .split(' ')
+                        .map(|x| {
+                            x.trim()
+                                .parse::<i64>()
+                                .unwrap()})
+                        .collect();
+                    temp_mapping.maps.push( DestinationMap { 
+                        source_start: *parsed_nums.iter().nth(1).unwrap(),
+                        dest_start: *parsed_nums.iter().nth(0).unwrap(),
+                        len: *parsed_nums.iter().nth(2).unwrap() });
+                }
+            }
+            // println!("pushing mapping: {:?}", temp_mapping);
+            all_maps.push(temp_mapping);
+        }
+    }
+    
+    // first part
+    // let mut locations: Vec<i64> = vec![];
+    // for seed in seeds {
+    //     let location = all_maps.clone().iter().fold(seed, |acc: i64, m| {
+    //         let temp = map_seed_to_destination(acc, &m.maps);
+    //         temp
+    //     });
+    //     locations.push(location);
+    // }
+
+    // println!("Lowest location: {}", locations.iter().min().unwrap());
+
+    let mut seed_ranges: Vec<Range<i64>> = vec![];
+    for idx in 0..(seeds.len()/2) {
+        seed_ranges.push(Range { 
+            start: seeds[idx*2],
+            end: seeds[idx*2] + seeds[idx*2 + 1],
+        })
+    }
+    let min_location = seed_ranges
+        .into_par_iter()
+        .flat_map(|range| range.clone())
+        .map(|seed|{
+            all_maps.iter()
+                .fold(seed, |seed, map| map_seed_to_destination(seed, map.maps.clone()))
+        })
+        .min();
+    println!("Min Location: {}", min_location.unwrap());
+    // BRUTE FORCE METHOD
+    // let mut locations: Vec<i64> = vec![];
+    // let num_loops = seeds.len() / 2;
+    // let mut handles = Vec::new();
+    // for i in 0..num_loops {
+    //     let seed_start = seeds[i*2];
+    //     let seed_end = seed_start + &seeds[i*2 + 1];
+    //     let map_ref = all_maps.clone();
+    //     println!("start: {}, end: {}",seed_start, seed_end);
+
+    //     handles.push(thread::spawn( move || {
+    //         get_lowest_from_seeds(&seed_start.clone(), &seed_end.clone(), &map_ref)
+    //     }));
+    // }
+    // for handle in handles {
+    //     locations.push(handle.join().unwrap());
+    // }
+    //println!("Lowest overall location: {}", locations.iter().min().unwrap());
+}
+
+pub fn get_lowest_from_seeds(start_seed: &i64, end_seed: &i64, maps: &Vec<Mapping>) -> i64 {
+    let mut lowest_location: Option<i64> = None;
+    for seed in *start_seed..*end_seed {
+        let location = maps.clone()
+            .iter()
+            .fold(seed, |acc: i64, m| {
+                let temp =  map_seed_to_destination(acc, m.maps.clone());
+                temp
+            });
+        if lowest_location.is_none() || lowest_location.unwrap() > location {
+            lowest_location = Some(location);
+        }
+    }
+    // println!("Lowest Location: {}", lowest_location.unwrap());
+    return lowest_location.unwrap();
+}
+
+#[derive(Debug, Clone)]
+pub struct Mapping {
+    source_name: String,
+    dest_name: String,
+    maps: Vec<DestinationMap>,
+}
+
+// DestinationMap define
+#[derive(Debug, Clone)]
+pub struct DestinationMap {
+    source_start: i64,
+    dest_start: i64,
+    len: i64,
+}
+
+pub fn map_seed_to_destination(seed: i64, dest_map: Vec<DestinationMap>) -> i64 {
+    let mut map_val: Option<i64> = None;
+
+    for map in dest_map {
+        if seed >= map.source_start && seed <= map.source_start + map.len - 1 {
+            map_val = Some(map.dest_start + (seed - map.source_start));
+        }
+    }
+
+    if map_val == None {
+        // println!("Map is none: {}", seed);
+        return seed
+    } else {
+        // println!("Map existed: {} -> {}", seed, map_val.unwrap());
+        return map_val.unwrap()
+    }
 }
