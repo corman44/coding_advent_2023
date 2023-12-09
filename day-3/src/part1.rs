4f8f4fad36911439dbf9173b1af1f24c00b64b53
Cory Johnson <cory1993johnson@gmail.com>
2023-12-09 10:51:13 +0100

    big changes: following chris' template

---
+++ day-3/src/part1.rs
@@ -0,0 +1,28 @@
+
+#[tracing::instrument]
+pub fn process(
+    _input: &str,
+) -> miette::Result<String, AocError> {
+    todo!("day 01 - part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!("4361", process(input)?);
+        Ok(())
+    }
+}
